3c7176361dfcef53da64f1fe95aa1f30786d8869
stuartraty <162755736+stuartraty@users.noreply.github.com>
2025-09-16 18:31:48 -0500

    completed hw file

--- image_op/operations.py
+++ image_op/operations.py
@@ -12,32 +12,32 @@ class Operation:
 
     def flip(self, image, direction="horizontal"):
         """
-          Perform image flipping along horizontal or vertical direction
+        Perform image flipping along horizontal or vertical direction
 
-          image: the input image to flip
-          direction: direction along which to flip
+        image: the input image to flip
+        direction: direction along which to flip
 
-          return: output_image
-          """
+        return: output_image
+        """
 
         rows, cols = shape(image)[:2]
         output_image = zeros(shape(image), uint8)
 
-        for r in range(rows):
-            for c in range(cols):
-                if direction == "horizontal":
-                    output_image[r][cols == 1 - c] = image [r][c]
-                elif direction == "vertical":
-                    output_image[rows - 1 - r][c] = image [r][c]
-                else:
-                    output_image[r][c] = image [r][c]
-                    raise Exception("Invalid direction")
+        if direction == "horizontal":
 
-        
+            for r in range(rows):
+                for c in range(cols):
+                    output_image[r][c] = image[r][cols - 1 - c]
+        elif direction == "vertical":
+
+            for r in range(rows):
+                for c in range(cols):
+                    output_image[r][c] = image[rows - 1 - r][c]
+        else:
+
+            output_image = image
 
         return output_image
-
-
     def chroma_keying(self, foreground, background, target_color, threshold):
         """
         Perform chroma keying to create an image where the targeted green pixels is replaced with
@@ -51,22 +51,38 @@ class Operation:
         return: output_image
         """
 
-        # add your code here
-        rows, cols = shape(foreground)[:2]
-        output_image = zeros(shape(foreground), uint8)
 
-        tr, tg, tb = target_color
-        for r in range(rows):
-            for c in range(cols):
-                fr, fg, fb = foreground[r][c]
-                distance = math.sqrt((fr - tg) ** 2 + (fb - tb) ** 2)
+        height = len(foreground)
+        width = len(foreground[0])
 
-                if distance < threshold:
-                    output_image[r][c] = background[r][c]
+
+        output_image = zeros((height, width, 3), dtype=uint8)
+
+
+        threshold_sq = threshold * threshold
+
+
+        target_r, target_g, target_b = target_color
+
+        for y in range(height):
+            for x in range(width):
+
+                fg_pixel = foreground[y][x]
+                fg_r, fg_g, fg_b = int(fg_pixel[0]), int(fg_pixel[1]), int(fg_pixel[2])
+
+
+                distance_sq = ((fg_r - target_r) ** 2 +
+                               (fg_g - target_g) ** 2 +
+                               (fg_b - target_b) ** 2)
+
+
+                if distance_sq <= threshold_sq:
+                    output_image[y][x] = background[y][x]
                 else:
-                    output_image[r][c] = foreground[r][c]
+                    output_image[y][x] = foreground[y][x]
 
-        # Please do not change the structure
-        return  output_image # Currently the input image is returned, please replace this with the color extracted image
+        return output_image
+    # Please do not change the structure
+         # Currently the input image is returned, please replace this with the color extracted image
 
    
